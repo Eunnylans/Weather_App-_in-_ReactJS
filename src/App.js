3c2989eda3dd1158ea787c15fbaddb06310277f1
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-07-30 20:19:42 +0200

    Updating App Functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+const api = {
+  key: "54c9845255068b24ff06aae5ad0ab5d1",
+  base: "https://api.openweathermap.org/data/3.0/",
+};
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednessday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
   return (
-    <div className="App">
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">Bayern Munich, Germany</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
     </div>
   );
 }
